10fb6e0e1fd9ec1e39ea6cf60449a4401bb333e3
Leslie Liu <s9172051212@gmail.com>
2024-10-10 05:23:10 +0000

    update task2.sh

--- task2.sh
+++ task2.sh
@@ -5,6 +5,4 @@ grep -rl 'sample' dataset1/ | xargs -I {} sh -c 'count=$(grep -o "CSC510" "{}" |
 #task b
 gawk '{print $1, $2}' | xargs -I {} sh -c 'size=$(stat -f "%z" "$(echo {} | cut -d" " -f2)"); echo "$(echo {} | cut -d" " -f1) $size $(echo {} | cut -d" " -f2)"' | sor    t -k1,1nr -k2,2nr | \
 #task c
-sed 's/file_/filtered_/'
-
-
+sed 's/dataset1\/\///g' | sed 's/file_/filtered_/'
