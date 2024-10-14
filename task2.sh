b4ea24afc3633cdd870aeb945178131872b2db5f
Leslie Liu <s9172051212@gmail.com>
2024-10-14 14:26:58 +0000

    edit task2

--- task2.sh
+++ task2.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-grep -rl 'sample' dataset1/ | xargs -I {} sh -c 'count=$(grep -o "CSC510" "{}" | wc -l); if [ "$count" -ge 3 ]; then echo "$count {}"; fi' | gawk '{print $1, $2}' | xargs -I {} sh -c 'size=$(stat -f "%z" "$(echo {} | cut -d" " -f2)"); echo "$(echo {} | cut -d" " -f1) $size $(echo {} | cut -d" " -f2)"' | sor    t -k1,1nr -k2,2nr | sed 's/dataset1\/\///g' | sed 's/file_/filtered_/'
+grep -rl 'sample' dataset1/ | xargs -I {} sh -c 'count=$(grep -o "CSC510" "{}" | wc -l); if [ "$count" -ge 3 ]; then echo "$count {}"; fi' | gawk '{print $1, $2}' | xargs -I {} sh -c 'size=$(stat -f "%z" "$(echo {} | cut -d" " -f2)"); echo "$(echo {} | cut -d" " -f1) $size $(echo {} | cut -d" " -f2)"' | sort -k1,1nr -k2,2nr | sed 's/dataset1\/\///g' | sed 's/file_/filtered_/'
